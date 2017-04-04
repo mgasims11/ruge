5fc9f3bdf9d1be15456b994f146978161559b38f
mgasims11 <mgasims@hotmail.com>
2017-04-03 21:52:51 -0400

    Worked on HandEvaluator. Clarified tests

--- Ruge.Win.Test/CardEngine.Test/UnitTest1.cs
+++ Ruge.Win.Test/CardEngine.Test/UnitTest1.cs
@@ -10,17 +10,17 @@ namespace CardEngine.Test
         [TestMethod]
         public void TestMethod1()
         {
-            var pr = new CardEngine.Logic.HandEvaluator();
+            var he = new CardEngine.Logic.HandEvaluator();
 
-            pr.Evaluate()
-            var result = pr.GetMatches(new HandEvaluator.Card[] {
-                new HandEvaluator.Card() { Value = 9, Suit = 3 },
-                new HandEvaluator.Card() { Value = 7, Suit = 3 },
-                new HandEvaluator.Card() { Value = 7, Suit = 3 },
-                new HandEvaluator.Card() { Value = 10, Suit = 3 },
-                new HandEvaluator.Card() { Value = 10, Suit = 3 }
-                }
-            );
+            Assert.AreEqual(he.Evaluate(1, 2, 8, 3, 3, 2, 4, 5, 5, 1), PokerRanking.HighCard);
+            Assert.AreEqual(he.Evaluate(2, 2, 4, 2, 6, 2, 8, 2, 10, 2), PokerRanking.Flush);
+            Assert.AreEqual(he.Evaluate(7, 2, 4, 0, 6, 2, 7, 2, 10, 2), PokerRanking.Pair);
+            Assert.AreEqual(he.Evaluate(7, 1, 4, 2, 6, 2, 7, 2, 4, 2), PokerRanking.TwoPair);
+            Assert.AreEqual(he.Evaluate(7, 2, 4, 2, 7, 2, 7, 2, 4, 2), PokerRanking.FullHouse);
+            Assert.AreEqual(he.Evaluate(7, 2, 4, 2, 7, 2, 7, 2, 5, 1), PokerRanking.ThreeOfAKind);
+            Assert.AreEqual(he.Evaluate(7, 2, 7, 2, 7, 2, 7, 2, 5, 1), PokerRanking.FourOfAKind);
+            Assert.AreEqual(he.Evaluate(7, 2, 8, 9, 9, 2, 10, 2, 11, 1), PokerRanking.Straight);
+            Assert.AreEqual(he.Evaluate(10, 2, 11, 2, 12, 2, 13, 2, 14, 2), PokerRanking.RoyalFlush);
         }
     }
 }
--- cardEngine.lib/Logic/HandEvaluator.cs
+++ cardEngine.lib/Logic/HandEvaluator.cs
@@ -27,18 +27,35 @@ namespace CardEngine.Logic
             public int Suit { get; set; }
         }
 
+
+        public PokerRanking Evaluate(
+            int rank1, int suit1,
+            int rank2, int suit2,
+            int rank3, int suit3,
+            int rank4, int suit4,
+            int rank5, int suit5
+            )
+        {
+            return Evaluate(
+            new HandEvaluator.Card() { Value = rank1, Suit = suit1 },
+            new HandEvaluator.Card() { Value = rank2, Suit = suit2 },
+            new HandEvaluator.Card() { Value = rank3, Suit = suit3 },
+            new HandEvaluator.Card() { Value = rank4, Suit = suit4 },
+            new HandEvaluator.Card() { Value = rank5, Suit = suit5 }
+            );
+        }
+
         public PokerRanking Evaluate(params Card[] hand)
         {
-
             if (IsRoyalFlush(hand)) return PokerRanking.RoyalFlush;
-            if (IsStraightFlush(hand)) return PokerRanking.RoyalFlush;
-            if (IsFourOfAKind(hand)) return PokerRanking.RoyalFlush;
-            if (IsFullHouse(hand)) return PokerRanking.RoyalFlush;
-            if (IsFlush(hand)) return PokerRanking.RoyalFlush;
-            if (IsStraight(hand)) return PokerRanking.RoyalFlush;
-            if (IsThreeOfAKind(hand)) return PokerRanking.RoyalFlush;
-            if (IsTwoPair(hand)) return PokerRanking.RoyalFlush;
-            if (IsPair(hand)) return PokerRanking.RoyalFlush;
+            if (IsStraightFlush(hand)) return PokerRanking.StraightFlush;
+            if (IsFourOfAKind(hand)) return PokerRanking.FourOfAKind;
+            if (IsFullHouse(hand)) return PokerRanking.FullHouse;
+            if (IsFlush(hand)) return PokerRanking.Flush;
+            if (IsStraight(hand)) return PokerRanking.Straight;
+            if (IsThreeOfAKind(hand)) return PokerRanking.ThreeOfAKind;
+            if (IsTwoPair(hand)) return PokerRanking.TwoPair;
+            if (IsPair(hand)) return PokerRanking.Pair;
 
             return PokerRanking.HighCard; ;
         }
@@ -134,31 +151,37 @@ namespace CardEngine.Logic
         public bool IsPair(Card[] hand)
         {
             var matches = GetMatches(hand);
-            return (matches.Count == 1 && matches[0] == 2);
+            var x = matches.Where(m => m.Value == 2);
+            return (x.Count() == 1);
         }
 
         public bool IsTwoPair(Card[] hand)
         {
             var matches = GetMatches(hand);
-            return (matches.Count == 2 && matches[0] == 2 && matches[1] == 2);
+            var x = matches.Where(m => m.Value == 2);
+            return (x.Count() == 2);
         }
 
         public bool IsFullHouse(Card[] hand)
         {
             var matches = GetMatches(hand);
-            return (matches.Count == 2 && matches[0] == 2 && matches[1] == 3);
+            var x = matches.Where(m => m.Value == 2);
+            var y = matches.Where(m => m.Value == 3);
+            return (x.Count() == 1 && y.Count() == 1);
         }
 
         public bool IsFourOfAKind(Card[] hand)
         {
             var matches = GetMatches(hand);
-            return (matches.Count == 1 && matches[0] == 4);
+            var x = matches.Where(m => m.Value == 4);
+            return (x.Count() == 1);
         }
 
         public bool IsThreeOfAKind(Card[] hand)
         {
             var matches = GetMatches(hand);
-            return (matches.Count == 1 && matches[0] == 3);
+            var x = matches.Where(m => m.Value == 3);
+            return (x.Count() == 1);
         }
 
     }
